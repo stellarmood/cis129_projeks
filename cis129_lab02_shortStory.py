27e02c05853d02852a941aa1d3153664e1bf8c1e
stellarmood <jfeddick@mail.pima.edu>
2024-06-17 12:23:35 -0700

    Update cis129_lab02_shortStory.py
    a short story has nothing to do with anything based in reality

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,3 @@
-print('hello, world!')
+print('The trip to Disneyworld had really cheered me up after yet another miscarriage.')
+print('Your parents don't want you anymore, that's why we're your new parents." I heard myself say to the girl who was now in our backseat.')
+print('wee woo sirens LOL')
