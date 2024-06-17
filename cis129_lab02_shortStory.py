520a765a763953ff91cd07c3b8abadc6087930b1
stellarmood <jfeddick@mail.pima.edu>
2024-06-17 12:26:24 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,3 +1,5 @@
+#code prints a short horror story
+#author jp
 print('The trip to Disneyworld had really cheered me up after yet another miscarriage.')
 print('Your parents do not want you anymore, thats why were your new parents. I heard myself say to the girl who was now in our backseat.')
 print('wee woo sirens LOL')
