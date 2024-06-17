2575847bcd55b432d5e5e4497e4e850601cfa311
stellarmood <jfeddick@mail.pima.edu>
2024-06-17 12:24:45 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,3 +1,3 @@
 print('The trip to Disneyworld had really cheered me up after yet another miscarriage.')
-print('Your parents do not want you anymore, thats why we're your new parents. I heard myself say to the girl who was now in our backseat.')
+print('Your parents do not want you anymore, thats why were your new parents. I heard myself say to the girl who was now in our backseat.')
 print('wee woo sirens LOL')
